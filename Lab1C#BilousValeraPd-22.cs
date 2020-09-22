eea6d92615178bb9824c021eb97b9927ab8b4c03
Polilen <71723246+Polilen@users.noreply.github.com>
2020-09-22 20:44:59 +0300

    Add files via upload

---
+++ Lab1C#BilousValeraPd-22.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int u = 50;
+        Console.WriteLine("напряжение=" + u);
+        int i = 10;
+        Console.WriteLine("сила тока = " + i);
+        int r;
+        r = u / i;
+        Console.WriteLine("сопротивление = " + r);
+    }
+}
