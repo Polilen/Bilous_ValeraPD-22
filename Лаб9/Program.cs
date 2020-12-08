bd89115f0aabb9952220c414e1015f09a5e752ab
Polilen <71723246+Polilen@users.noreply.github.com>
2020-12-08 20:24:30 +0200

    Add files via upload

---
+++ Лаб9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace Lab9
+{
+
+    interface IDraw
+    {
+        void Draw();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Circle circle = new Circle("Круг 1");
+
+            Quadrate quadrate = new Quadrate("Квадрат 1", "синий", 25);
+
+            Triangle triangle = new Triangle("Треугольник 1", 10, 15);
+
+            Picture picture = new Picture();
+
+            picture.Add(circle);
+            picture.Add(quadrate);
+            picture.Add(triangle);
+            picture.Add(new Quadrate("Квадрат", "зеленый", 100));
+
+            picture.Draw();
+
+            Console.WriteLine("-------------------");
+
+            picture.RemoveByName("Круг 1");
+            picture.RemoveByArea(625);
+            picture.RemoveByType(typeof(Circle));
+
+            picture.Draw();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Painter.Draw(circle);
+            Painter.Draw(quadrate);
+            Painter.Draw(picture);
+
+
+        }
+
+    }
+
+}
---
+++ Лаб9/circle.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Lab9
+{
+    class Circle : Shape, IDraw
+    {
+        private double circleRadius;
+        private string name;
+        public double Radius
+        {
+            get { return circleRadius; }
+            set
+            {
+                if (value <= 0)
+                {
+                    circleRadius = 1;
+                }
+                else
+                {
+                    circleRadius = value;
+                }
+            }
+        }
+
+        public override int NumOfVertices { get { return 0; } }
+
+        public override string Name { get { return name; } }
+
+        public override string FigureColor { get; set; }
+
+        public Circle(string name, string color, double radius)
+        {
+            this.name = name;
+            FigureColor = color;
+            Radius = radius;
+        }
+        public Circle(string name, double radius)
+        {
+            this.name = name;
+            Radius = radius;
+            FigureColor = RandomColor.randomColor();
+        }
+        public Circle(string name)
+        {
+            this.name = name;
+            FigureColor = RandomColor.randomColor();
+            Random r = new Random();
+            Radius = r.Next(100);
+        }
+
+        public override double Square()
+        {
+            double S = Math.PI * Math.Pow(Radius, 2);
+            return S;
+        }
+
+        public override double Perimeter()
+        {
+            double P = 2 * Math.PI * Radius;
+            return P;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Имя: {0}\nЦвет: {1}\nРадиус: {2}\nПлощадь: {3}\nПериметр: {4}",
+                Name, FigureColor, Radius, Square(), Perimeter());
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Почти круг");
+            Console.WriteLine("-------------------------------------");
+        }
+
+
+    }
+
+}
