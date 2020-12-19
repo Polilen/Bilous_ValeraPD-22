fef5284be048d4ebe1d3b6fca960e7058f569ca1
Polilen <71723246+Polilen@users.noreply.github.com>
2020-12-19 17:44:51 +0200

    Add files via upload

---
+++ Lab11/Lab11/Extension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_11_BilousValera
+{
+    static class Extension
+    {
+        public static List<Student> FindStudent(this List<Student> list, StudentPredicateDelegate spd)
+        {
+            List<Student> reqList = new List<Student>();
+            foreach (Student st in list)
+            {
+                if (spd.Invoke(st))
+                    reqList.Add(st);
+            }
+            return reqList;
+        }
+
+        public static void DisplayList(this List<Student> list)
+        {
+
+            foreach (Student st in list)
+            {
+                Console.WriteLine("Имя: {0}", st.FirstName);
+                Console.WriteLine("Фамилия: {0}", st.LastName);
+                Console.WriteLine("Возраст: {0}", st.Age);
+            }
+        }
+    }
+}
---
+++ Lab11/Lab11/patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_11_BilousValera
+{
+    public class patient
+    {
+        public bool MRT { get; set; }
+        public bool CT { get; set; }
+        public bool Ren { get; set; }
+        public bool examinationlor { get; set; }
+        public bool Ultrasound { get; set; }
+        public bool rheumatictests { get; set; }
+
+    }
+}
