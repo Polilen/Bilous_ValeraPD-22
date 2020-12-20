76230d13ecc5cf8a20b4db37e15cb120fa8aeb99
Polilen <71723246+Polilen@users.noreply.github.com>
2020-12-20 15:32:18 +0200

    Add files via upload

---
+++ Sr/Sr/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sr
+{
+    class Car
+    {
+        public List<String> colors = new List<String>();
+        public int yearOfMade;
+        public int price;
+        public int enginePower;
+
+        public Car(string[] colors = null, int yom = 0, int price = 0, int ep = 0)
+        {
+            for (var i = 0; i < colors.Length; i++)
+                this.colors.Add(colors[i]);
+            yearOfMade = yom;
+            this.price = price;
+            enginePower = ep;
+        }
+        public void showInfo()
+        {
+            Console.WriteLine();
+            Console.Write($"Доступние цвета: ");
+            foreach (var color in colors)
+                Console.Write($" {color} ");
+            Console.WriteLine();
+            Console.WriteLine($"Год производства: {yearOfMade} год");
+            Console.WriteLine($"Цена: {price}грн.");
+            Console.WriteLine($"Мощьность двигателя: {enginePower}");
+        }
+    }
+}
---
+++ Sr/Sr/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace sr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = new Cars();
+            bool exist = true;
+
+            string color;
+            int yearOfMade = 0;
+            int price = 0;
+            int enginePower = 0;
+
+            byte amount = 0;
+            string tempForInput;
+
+            
+            Console.WriteLine("Добрый день , привествую вас в магазине Polilen");
+            Console.WriteLine("Хотите ли вы выбирать магину по параметрам?");
+            Console.WriteLine("{Yes(Да, хочу), No(Нет, не хочу , хочу увидеть все варианты)}");
+            Console.Write("Введите Yes или No: ");
+            tempForInput = Console.ReadLine();
+
+            if (tempForInput == "Yes")
+            {
+                while (exist)
+                {
+                    amount = 0;
+                    color = null;
+                    tempForInput = null;
+
+                    Console.WriteLine();
+                    Console.WriteLine("Вы можете вибрать себе машину по таких параметрах");
+                    Console.Write("{Цвет(");
+                    Console.Write("На английском");
+                    Console.Write("), Год производства, Цена и Мощьность двигателя} \n");
+                    Console.Write("Если ви хотите пропустить какую-то характеристику введите - \n\n");
+
+                    Console.Write("Введите цвет авто: ");
+                    color = Console.ReadLine();
+                    if (color == "-")
+                        amount++;
+
+                    Console.Write("Введите год производства: ");
+                    tempForInput = Console.ReadLine();
+                    if (tempForInput == "-")
+                    {
+                        tempForInput = null;
+                        amount++;
+                    }
+                    else yearOfMade = Convert.ToInt32(tempForInput);
+
+                    Console.Write("Введите желаемую цену: ");
+                    tempForInput = Console.ReadLine();
+                    if (tempForInput == "-")
+                    {
+                        tempForInput = null;
+                        amount++;
+                    }
+                    else price = Convert.ToInt32(tempForInput);
+                    Console.Write("Введите желаемую мощьность двигателя: ");
+                    tempForInput = Console.ReadLine();
+                    if (tempForInput == "-")
+                    {
+                        tempForInput = null;
+                        amount++;
+                    }
+                    else enginePower = Convert.ToInt32(tempForInput);
+
+                    cars.findCar(amount, color, yearOfMade, price, enginePower);
+                    Console.WriteLine("\nМне не нравлятся эти машины , хочу сделать выбрать еще раз! ");
+                    Console.WriteLine("{Yes, No(Виход из программи)}");
+                    Console.Write("Введите Yes или No: ");
+                    tempForInput = Console.ReadLine();
+                    exist = tempForInput == "Yes" ? true : false;
+                }
+            }
+            else
+            {
+                cars.showInfoAboutCars();
+                Console.WriteLine();
+                exist = false;
+            }
+
+        }
+    }
+}
