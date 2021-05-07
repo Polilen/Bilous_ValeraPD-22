4f708e1ec022c7c12772d3142eff2a67a45804d6
Polilen <71723246+Polilen@users.noreply.github.com>
2021-05-07 16:18:32 +0300

    Add files via upload

---
+++ Lab5WPFValeraBilousPD-22/lab-2_(WPF)/ButtonField.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace lab_2__ValeraBilousPD22
+{
+    public class ButtonsField
+    {
+        private List<Button> Buttons = new List<Button>();
+        private sbyte amountButtons = 16;
+        private sbyte count = 0;
+        private object textBoxForResult;
+
+        public ButtonsField(object textBoxForResult)
+        {
+            for (int i = 0; i < amountButtons; i++)
+            {
+                Buttons.Add(new Button());
+
+                Buttons[i].Content = (i + 1).ToString();
+
+                Buttons[i].Width = 40;
+                Buttons[i].Height = 40;
+
+                Buttons[i].Click += btn_handler;
+            }
+
+            this.textBoxForResult = (textBoxForResult);
+        }
+
+        public void drawField(Grid getter)
+        {
+            sbyte x = 0, y = 0;
+
+            for (int i = 0; i < amountButtons; i++)
+            {
+                Grid.SetColumn(Buttons[i], 0 + x);
+                Grid.SetRow(Buttons[i], 0 + y);
+                getter.Children.Add(Buttons[i]);
+
+                x++;
+                if ((i + 1) % 4 == 0)
+                {
+                    y++;
+                    x = 0;
+                }
+            }
+        }
+
+        private void resetHidingButtons()
+        {
+            for (int i = 0; i < amountButtons; i++)
+                if (Buttons[i].Visibility == Visibility.Hidden)
+                    Buttons[i].Visibility = Visibility.Visible;
+            count = 0;
+        }
+
+        private void btn_handler(object sender, EventArgs e)
+        {
+            if (count == (amountButtons - 1))
+            {
+                (textBoxForResult as TextBox).Text = "Красава!!";
+                resetHidingButtons();
+            }
+            else
+                (textBoxForResult as TextBox).Text = "";
+
+            if ((sender as Button).Content.ToString() == (count + 1).ToString())
+            {
+                (sender as Button).Visibility = Visibility.Hidden;
+                count++;
+            }
+            else
+            {
+                resetHidingButtons();
+                count = 0;
+            }
+        }
+    }
+}
---
+++ Lab5WPFValeraBilousPD-22/lab-2_(WPF)/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace lab_2__ValeraBilousPD22
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ButtonsField bf = new ButtonsField(textBox2);
+            bf.drawField(gridForButtons);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text != "")
+            {
+                comboBox.Items.Add(textBox.Text);
+                textBox.Text = "";
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (comboBox.Items.Count != 0)
+                comboBox.Items.RemoveAt(comboBox.Items.Count - 1);
+           
+        }
+    }
+}
